42cde4062e290d9c412c8628cc26ad3eea59887c
brunocardosof <bruno.cardosof@gmail.com>
2021-04-18 03:14:29 -0300

    refactor product list import on routes

--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import ProductList from './pages/ProductList';
+import ProductList from './screens/ProductList';
 
 const RouteStack = createStackNavigator();
 
