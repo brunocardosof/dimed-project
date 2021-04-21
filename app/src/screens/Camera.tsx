29acc072b4549a78944fa8bf085b4275636774c2
BrunoSoftnew <bruno@softnew.com.br>
2021-04-21 18:38:01 -0300

    add redux to camera component
    add get product method based on barcode reader ean

--- app/src/screens/Camera.tsx
+++ app/src/screens/Camera.tsx
@@ -1,14 +1,24 @@
 'use strict';
 import React, {PureComponent} from 'react';
 import {BarCodeReadEvent, RNCamera} from 'react-native-camera';
+import {connect} from 'react-redux';
 import {Container} from '../components/Camera';
+import {Product} from '../interfaces/Product';
+
+interface State {
+  fullDataProducts: Product[];
+}
 
 class Camera extends PureComponent<any, any> {
   onBarCodeRead = (e: BarCodeReadEvent) => {
-    this.props.navigation.navigate('ProductDetail', {
-      data: e.data,
-      type: e.type,
+    const product = this.getProduct(Number(e.data));
+    this.props.navigation.navigate('ProductDetail', {product});
+  };
+  getProduct = (ean: number): Product => {
+    const product = this.props.fullDataProducts.filter((item: Product) => {
+      return item.ean === ean;
     });
+    return product;
   };
   render() {
     return (
@@ -40,4 +50,10 @@ class Camera extends PureComponent<any, any> {
   }
 }
 
-export default Camera;
+const mapStateToProps = (state: State) => {
+  return {
+    fullDataProducts: state.fullDataProducts,
+  };
+};
+
+export default connect(mapStateToProps, null)(Camera);
